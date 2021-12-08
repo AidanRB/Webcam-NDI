719d8b9ef8fea5b4c711f7ed929f8229d2246d16
AidanRB <aidanrichbennett@gmail.com>
2021-12-08 11:13:36 -0500

    [INCOMPLETE] Added streaming to NDI from OpenCV Segfaults half the time Possible interlacing/format problems Some code taken from: https://github.com/xseignard/ndi-test/blob/master/main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <cstdio>
+#include <iostream>
 #include <cstdlib>
 #include <cstring>
 #include <chrono>
 #include <Processing.NDI.Lib.h>
-// #include "opencv-build"
+#include <opencv2/opencv.hpp>
 
 #ifdef _WIN32
 #ifdef _WIN64
@@ -14,21 +14,47 @@
 #endif // _WIN32
 
 int main(int argc, char* argv[])
-{	// Not required, but "correct" (see the SDK documentation.
+{	
+	const int X = 640;
+	const int Y = 480;
+	
+	// Not required, but "correct" (see the SDK documentation).
 	if (!NDIlib_initialize()) return 0;
 	
-	// We create the NDI sender
+	// create the NDI sender
 	NDIlib_send_instance_t pNDI_send = NDIlib_send_create();
 	if (!pNDI_send) return 0;
 
-	// We are going to create a 1920x1080 interlaced frame at 60.00Hz.
+
+	// NDI frame initialization
 	NDIlib_video_frame_v2_t NDI_video_frame;
-	NDI_video_frame.xres = 1920;
-	NDI_video_frame.yres = 1080;
-	NDI_video_frame.FourCC = NDIlib_FourCC_type_BGRX;
-	NDI_video_frame.p_data = (uint8_t*)malloc(NDI_video_frame.xres*NDI_video_frame.yres * 4);
-	NDI_video_frame.frame_rate_N = 60000;
-	NDI_video_frame.frame_rate_D = 1000;
+	// resolution
+	NDI_video_frame.xres = X;
+	NDI_video_frame.yres = Y;
+	// type
+	NDI_video_frame.FourCC = NDIlib_FourCC_type_BGRA;
+	// allocate memory for the actual data
+	// NDI_video_frame.p_data = (uint8_t*)malloc(X * Y * 4);
+	// framerate (N/D)/s
+	NDI_video_frame.frame_rate_N = 60;
+	NDI_video_frame.frame_rate_D = 1;
+
+
+	// OpenCV initialization
+	cv::Mat frame;
+	cv::Mat NDIframe;
+    cv::VideoCapture cap;
+	// resolution of capture
+	cap.set(cv::CAP_PROP_FRAME_WIDTH, X);
+	cap.set(cv::CAP_PROP_FRAME_HEIGHT, Y);
+    // open the default camera using default API
+    cap.open(0);
+    // check if we succeeded
+    if (!cap.isOpened()) {
+        std::cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+
 
 	// Run for one minute
 	using namespace std::chrono;
@@ -39,14 +65,21 @@ int main(int argc, char* argv[])
 		// Send 200 frames
 		for (int idx = 200; idx; idx--)
 		{	// Fill in the buffer. It is likely that you would do something much smarter than this.
-			memset((void*)NDI_video_frame.p_data, (idx & 1) ? 255 : 0, NDI_video_frame.xres*NDI_video_frame.yres * 4);
+			// memset((void*)NDI_video_frame.p_data, (idx & 1) ? 255 : 0, NDI_video_frame.xres*NDI_video_frame.yres * 4);
 
-			// We now submit the frame. Note that this call will be clocked so that we end up submitting at exactly 29.97fps.
+			// something much smarter than that^
+			cap.read(frame);
+			NDI_video_frame.p_data = (uint8_t*)frame.data;
+			// send the frame
 			NDIlib_send_send_video_v2(pNDI_send, &NDI_video_frame);
+
+			// show the opencv fram captured from the camera
+			cv::imshow("output", frame);
+			cv::waitKey(1);
 		}
 
 		// Just display something helpful
-		printf("200 frames sent, at %1.2ffps\n", 200.0f / duration_cast<duration<float>>(high_resolution_clock::now() - start_send).count());
+		std::cout << "200 frames sent, at " << 200.0 / duration_cast<duration<float>>(high_resolution_clock::now() - start_send).count() << "fps" << std::endl;
 	}
 
 	// Free the video frame
